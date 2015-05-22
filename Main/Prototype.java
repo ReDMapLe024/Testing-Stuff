d093ca224bb5f8f6895b5e356417c8cf6f08df59
ReDMapLe024 <pfillion024@gmail.com>
2015-05-22 13:15:28 -0400

    Update and rename entity.java to Main/Prototype.java

---
+++ Main/Prototype.java
@@ -0,0 +1,17 @@
+public abstract class Prototype[
+    
+    public class Prototype{
+      public static void main (String [] args){
+        
+        JFrame gameFrame = new JFrame("Prototype");
+        gameFrame.add(new protoPanel());
+        gameFrame.setResizable(false);
+        gameFrame.pack();
+        gameFrame.setPositionRelativeTo(null);
+        gameFrame.setVisible(true);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+      }
+    }
+    
+}
